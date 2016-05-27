7ed97335360222aba04ef917c7343b1206d0ee98
chin24 <chin2413@gmail.com>
2016-05-27 16:25:46 -0500

    Movement Mechanics implemented and Lighting started
    Finished adding the movement mechanics and border mechanics. Started
    Adding lighting effects to the game, but it needs to be optimized

--- Bubble Life/Assets/Scripts/BasePlayer.cs
+++ Bubble Life/Assets/Scripts/BasePlayer.cs
@@ -9,6 +9,7 @@ public class BasePlayer : MonoBehaviour {
 	public float width = 1.8f;
 	protected GameManager gameManager;
 	protected float speedMod = 40;
+	protected float wallForce = 1;
 	protected Text info;
 	protected Rigidbody2D rigid;
 	protected Vector3 offset;
@@ -28,12 +29,30 @@ public class BasePlayer : MonoBehaviour {
 		info = transform.GetComponentInChildren<Text> ();
 		rigid = gameObject.GetComponent<Rigidbody2D> ();
 	}
+
+	void FixedUpdate(){
+		
+		if (transform.position.x < 0) {
+			offset = new Vector3 (wallForce, 0);
+			rigid.AddForce (offset);
+		} else if (transform.position.y < 0) {
+			offset = new Vector3 (0, wallForce);
+			rigid.AddForce (offset);
+		} else if (transform.position.x > gameManager.borderSize) {
+			offset = new Vector3 (-wallForce, 0);
+			rigid.AddForce (offset);
+		} else if (transform.position.y > gameManager.borderSize) {
+			offset = new Vector3 (0, -wallForce);
+			rigid.AddForce (offset);
+		}
+	}
 	
     protected void Move(Vector3 target) {
         offset = target - transform.position;
         offset.Normalize();
-        GameObject obj = Instantiate(gameManager.food, transform.position + offset, Quaternion.identity) as GameObject;
-        obj.GetComponent<Rigidbody2D>().AddForce(offset * speedMod);
+        GameObject obj = Instantiate(gameManager.food, transform.position + offset * width/1.7f, Quaternion.identity) as GameObject;
+		obj.GetComponent<Rigidbody2D> ().velocity = rigid.velocity;
+        obj.GetComponent<Rigidbody2D> ().AddForce(offset * speedMod);
         rigid.AddForce(offset * -speedMod);
         BasePlayer objPlayer = obj.AddComponent<BasePlayer>();
         food--;
@@ -47,12 +66,16 @@ public class BasePlayer : MonoBehaviour {
 
 	
 		if (col.gameObject.tag != "border") {
-			BasePlayer colScript = col.gameObject.GetComponent<BasePlayer> ();
+			BasePlayer colScript = col.GetComponent<BasePlayer> ();
 			if (colScript.width < width && col.gameObject.tag != "Player") {
 				food += colScript.food;
+
+				Rigidbody2D colRigid = col.GetComponent<Rigidbody2D> ();
+				Vector2 newVelocity = InelasticCollision (rigid.mass, rigid.velocity, colRigid.mass, colRigid.velocity);
                 gameManager.AddFood(1);
 				Destroy (col.gameObject);
 				UpdateSize ();
+				rigid.velocity = newVelocity;
                 if (transform.tag != "food")
                 {
                     info.text = food.ToString();
@@ -62,6 +85,12 @@ public class BasePlayer : MonoBehaviour {
         }
 
 		
+	}
+
+	protected Vector2 InelasticCollision (float mass1, Vector2 vel1, float mass2, Vector2 vel2){
+
+		Vector2 finalVel = (mass1 * vel1 + mass2 * vel2) / (mass1 + mass2);
+		return finalVel;
 	}
 
 	protected void Attack(){
